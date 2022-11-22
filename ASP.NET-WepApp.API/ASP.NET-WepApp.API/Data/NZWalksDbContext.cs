4a20a3d28fd169670c27386389e14c6c057ce286
mirec <antunovic.mirko@gmail.com>
2022-11-22 16:16:20 +0100

    End of section 2

---
+++ ASP.NET-WepApp.API/ASP.NET-WepApp.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,24 @@
+﻿using ASP.NET_WepApp.API.Models.Domein;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ASP.NET_WepApp.API.Data
+{
+    public class NZWalksDbContext: DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        {
+
+
+
+        }
+        
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+
+
+    
+    }
+}
---
+++ ASP.NET-WepApp.API/ASP.NET-WepApp.API/Models/Domein/WalkDifficulty.cs
@@ -0,0 +1,13 @@
+﻿namespace ASP.NET_WepApp.API.Models.Domein
+{
+    public class WalkDifficulty
+    {
+
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+     
+
+
+
+    }
+}
