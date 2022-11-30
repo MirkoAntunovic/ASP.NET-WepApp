b85d95a88475a043bc56cf35b11743e3633d9ade
mirec <antunovic.mirko@gmail.com>
2022-11-30 22:43:03 +0100

    Added RegionsController

---
+++ ASP.NET-WepApp.API/ASP.NET-WepApp.API/Controllers/RegionsController.cs
@@ -0,0 +1,54 @@
+﻿using ASP.NET_WepApp.API.Models.Domein;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_WepApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+
+            var regions = new List<Region>()
+            {
+
+                new Region
+                {
+
+                    Id = Guid.NewGuid(),
+                  Name = "Zagreb",
+                  Code ="ZG",
+                  Area=323232,
+                  Lat=1.3232,
+                  Long=299.3,
+                  Population=90000
+
+                },
+
+                 new Region {
+
+
+                  Id = Guid.NewGuid(),
+                  Name = "Split",
+                  Code ="st",
+                  Area=323232,
+                  Lat=1.3232,
+                  Long=299.3,
+                  Population=90000
+
+              }
+
+
+
+
+            };
+
+            return Ok(regions);
+
+
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace ja
+{
+
+    class mirko
+    {
+        private string _name;
+        private string _lastname;
+
+        public mirko(string name, string lastname) {
+            _name = name;
+            _lastname = lastname;
+        
+        
+        
+        
+        }
+
+        public string Value { get; private set; }
+
+       public string name
+        {
+            get { return _name; }
+
+
+            set {_name=value; }
+
+
+        }
+
+        public string lastname
+        {
+            get { return _lastname; }
+
+
+            set { _lastname = value; }
+
+
+        }
+
+
+    }
+
+
+
+    class ja : mirko
+    {
+        public ja(string name, string lastname) : base(name, lastname)
+        {
+        }
+
+
+
+
+        public static void Main(string[] args)
+        {
+            ja j = new ja("Mirko","Antunovic");
+
+            Console.WriteLine("Vase ime je "+j.name+" a vase prezime je "+j.lastname);
+        }
+    }
+
+    
+
+}
